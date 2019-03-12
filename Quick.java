9fd8db6c99b4968ce5ce6f513dbfc78f0f816510
Kayla Fang <kfang00@stuy.edu>
2019-03-12 11:06:22 -0400

    easy improvements in partition

--- Quick.java
+++ Quick.java
@@ -1,12 +1,12 @@
-import java.util.*; 
+import java.util.*;
 import java.io.*;
 
 public class Quick {
 
 /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
- *2. A random index from start to end inclusive is chosen, the corresponding 
- *   element is designated the pivot element. 
+ *2. A random index from start to end inclusive is chosen, the corresponding
+ *   element is designated the pivot element.
  *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
  *@return the index of the final position of the pivot element.
@@ -14,12 +14,12 @@ public class Quick {
 
   public static int partition (int[] data, int s, int e){
     Random ran = new Random();
-    int ranI, pivot, start;
-    if ((e - s) != 0) {
-      ranI = (ran.nextInt() % (e - s)) + s;
+    int ranI, pivot, start, ranF;
+    if (e == s) {
+      ranI = s;
     }
     else {
-      ranI = e;
+      ranI = (e = s) / 2;
     }
     pivot = data[ranI];
     if (s != 0) {
@@ -34,14 +34,26 @@ public class Quick {
     data[ranI] = data[0];
     data[0] = pivot;
     while (start != end) {
+      ranF = Math.abs(ran.nextInt() % 2);
       if (data[start] > pivot) {
-	hold = data[start];
+	      hold = data[start];
         data[start] = data[end];
-	data[end] = hold;
+	      data[end] = hold;
         end = end - 1;
       }
       else if (data[start] < pivot) {
-	start += 1;
+	      start += 1;
+      }
+      else {
+        if (ranF == 0) {
+          hold = data[start];
+          data[start] = data[end];
+  	      data[end] = hold;
+          end = end - 1;
+        }
+        else {
+          start += 1;
+        }
       }
     }
     if (data[start] <= pivot) {
@@ -57,9 +69,9 @@ public class Quick {
     return pIdx;
   }
 
-/*return the value that is the kth smallest value of the array. 
+/*return the value that is the kth smallest value of the array.
  */
-  public static int quickselect(int []data, int k){ 
+  public static int quickselect(int []data, int k){
     return 0;
 
   }
